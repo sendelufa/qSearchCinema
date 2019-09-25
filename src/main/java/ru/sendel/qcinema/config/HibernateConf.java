68af819d1a2c52ec8312b7b02cf91667267770b0
shibkov.k <shibkov.k@gmail.com>
2019-09-25 09:12:25 +0500

    add Flyway migration\n Hibernate Java Config

---
+++ src/main/java/ru/sendel/qcinema/config/HibernateConf.java
@@ -0,0 +1,60 @@
+package ru.sendel.qcinema.config;
+
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateConf {
+
+   @Autowired
+   private Environment environment;
+   @Value("${spring.datasource.url}")
+   private String jdbcURl;
+   @Value("${spring.datasource.username}")
+   private String dbUsername;
+   @Value("${spring.datasource.password}")
+   private String dbPassword;
+   @Value("${spring.jpa.properties.hibernate.dialect}")
+   private String dbDialect;
+   @Value("${spring.jpa.hibernate.ddl-auto}")
+   private String ddlAuto;
+
+   @Bean
+   public LocalSessionFactoryBean sessionFactory() {
+      LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+      sessionFactory.setDataSource(dataSource());
+      sessionFactory.setHibernateProperties(hibernateProperties());
+      sessionFactory.setPackagesToScan("ru.sendel.qcinema.model");
+      return sessionFactory;
+   }
+
+   @Bean
+   public DataSource dataSource() {
+      DriverManagerDataSource dataSource = new DriverManagerDataSource();
+      dataSource.setUrl(jdbcURl);
+      dataSource.setUsername(dbUsername);
+      dataSource.setPassword(dbPassword);
+      return dataSource;
+   }
+
+   private Properties hibernateProperties() {
+      Properties hibernateProperties = new Properties();
+      hibernateProperties.setProperty(
+          "hibernate.hbm2ddl.auto", environment.getProperty("spring.jpa.hibernate.ddl-auto"));
+      hibernateProperties.setProperty(
+          "hibernate.dialect", environment.getProperty("spring.jpa.properties.hibernate.dialect"));
+      hibernateProperties.setProperty(
+          "hibernate.temp.use_jdbc_metadata_defaults", environment
+              .getProperty("spring.jpa.properties.hibernate.temp.use_jdbc_metadata_defaults"));
+      return hibernateProperties;
+   }
+}
---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,40 @@
+CREATE TABLE public.city (
+	"name" varchar(100) NOT NULL,
+	geo varchar(100) NOT NULL,
+	country text NULL,
+	id serial NOT NULL,
+	CONSTRAINT city_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE public.cinema (
+	id serial NOT NULL,
+	city serial NOT NULL,
+	"name" varchar(100) NOT NULL,
+	description text NULL,
+	geo varchar(100) NULL,
+	address varchar(100) NULL,
+	CONSTRAINT cinema_pk PRIMARY KEY (id),
+	CONSTRAINT cinema_fk FOREIGN KEY (city) REFERENCES public.city(id)
+);
+
+CREATE TABLE public.movie (
+	id serial8 NOT NULL,
+	name_native varchar(150) NOT NULL,
+	"year" varchar(4) NULL,
+	description text NULL,
+	poster_img text NULL,
+	name_original text NULL,
+	CONSTRAINT movie_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE public.movie_session (
+	id serial8 NOT NULL,
+	film_id serial NOT NULL,
+	cinema_id serial NOT NULL,
+	"date" date NOT NULL,
+	"cost" int NOT NULL,
+	hashcode varchar(32) NULL,
+	CONSTRAINT movie_session_pk PRIMARY KEY (id),
+	CONSTRAINT movie_session_film_fk FOREIGN KEY (film_id) REFERENCES public.movie(id),
+	CONSTRAINT movie_session_cinema_fk FOREIGN KEY (cinema_id) REFERENCES public.cinema(id)
+);
