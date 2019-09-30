f1a93dac09253020cd079f7fb7ea05a5887e2607
shibkov.k <shibkov.k@gmail.com>
2019-09-30 22:23:38 +0500

    init parser

--- src/main/java/ru/sendel/qcinema/parser/CinemaParser.java
+++ src/main/java/ru/sendel/qcinema/parser/CinemaParser.java
@@ -1,5 +1,10 @@
 package ru.sendel.qcinema.parser;
 
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -8,7 +13,25 @@ import org.springframework.scheduling.annotation.Scheduled;
 public class CinemaParser {
 
    @Scheduled(fixedDelay = 100)
-   private void parse(){
-    //TODO parse HTML
+   public void parse(String url) {
+      Document document;
+      try {
+         document =
+             Jsoup.connect(url)
+                 .userAgent("IE")
+                 .timeout(50000)
+                 .maxBodySize(0)
+                 .execute()
+                 .parse();
+      } catch (IOException e) {
+         e.printStackTrace();
+         return;
+         //TODO: log ex
+      }
+
+      Elements links = document.select("table.afisha-schedule tbody tr");
+
+      links.stream().map(Element::html).forEach(System.out::println);
+
    }
 }
