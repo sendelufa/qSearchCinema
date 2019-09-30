10f33ce2b2446b238c04466aa4cb2cb8273461f8
shibkov.k <shibkov.k@gmail.com>
2019-09-30 22:04:24 +0500

    Hibernate launch after Flyway

--- src/main/java/ru/sendel/qcinema/config/HibernateConf.java
+++ src/main/java/ru/sendel/qcinema/config/HibernateConf.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@DependsOn("flyway")
 @EnableTransactionManagement
 public class HibernateConf {
 
